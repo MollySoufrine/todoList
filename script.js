e6fc31324343545f896b44b27ba0c12e6a1be2a1
molly soufrine <mollysoufirne@gmail.com>
2020-06-17 18:06:31 -0400

    added jquery

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-  var scheduleText = 
+  var todoList = $(".form-control");
+  //class name of each table row this way I can access all of them
+
+  todoList.each(function () {
+    if (this.moment === "past") {
+      this.style.color = ".past";
+    } else if (this.moment === "future") {
+      this.style.color = ".future";
+    } else {
+      this.style.color = ".present";
+    }
+  });
 });
