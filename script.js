9165b30318204e04f35c2c1b19a8aef5e63fb1ea
molly soufrine <mollysoufirne@gmail.com>
2020-06-20 09:34:12 -0400

    created an if statement

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 $(document).ready(function () {
-  var date;
-  var time;
-  var storedTodo;
-  var button = $("#save-button");
+  var formControl = $(".form-control");
+  var time = moment().format("H");
+  var container = $(".container");
 
+  var showTodo;
+  var button = $(".save-button");
+  // var currentDate = $("#currentDay");
   //gives current date in my
   $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-  $(window).on("load", function () {
-    date = moment().format("YYYY-MM-DD, h:mm:ss a");
-    time = moment().format("H");
-  });
+  function tellTime() {
+    for (var i = 0; i <= 23; i++) {
+      if (time === i) {
+        $(".form-control").addClass("present");
+      } else if (time < i) {
+        $(".form-control").addClass("past");
+      } else {
+        $(".form-control").addClass("future");
+      }
+    }
+    console.log(time);
+  }
 
-  function savedTodo() {}
-  button.value = localStorage.getItem("");
-  button.click("input", function () {
-    localStorage.setItem("", button.value);
-  });
   tellTime();
-  // console.log(dateString);
 });
