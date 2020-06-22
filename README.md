75373dd0a5de509230f298fa44a96f397a9e2602
molly soufrine <mollysoufirne@gmail.com>
2020-06-22 17:56:46 -0400

    fixed the local storage

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # todoList
+
+This purpose of this project was to create a work scheudle or to-do list to save things on the hour that need to be accomplished.
+
+For future hours, the rows would be green, for the current hour the row would be red and for the past the rows would be grey.
+
+When the participant would write within the rows and click the save button the todo written would be saved and stay with that date and current time even if the page was refreshed.
+
+I was able to get the colors working for the hours, post the current date and time at the top of the screen but unable to have the info stay on the page if refreshed.
+
+it would show what i typed in localStorage when i checked it on the console.log but not stay on the page.
+I tried to adjust the getItem to just hour as well as set every hours to the simialr localStorage but that did not work either.
--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 $(document).ready(function () {
-  var hour = moment().hours();
+  // var hour = moment().hours();
+  $(".saveBtn").on("click", function () {
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, value);
+  });
+  // var showTodo = $(".todo");
 
-  var showTodo = $(".todo");
-
-  var button = $(".save-button");
+  // var button = $(".save-button");
   // var currentDate = $("#currentDay");
   //gives current date in my
-  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+  // $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
   var index = 0;
 
   function tellTime() {
+    var hour = moment().hours();
     for (i = 9; i <= 16; i++) {
       if (hour == i) {
         $($(".description")[index]).addClass("present");
@@ -19,30 +24,30 @@ $(document).ready(function () {
         $($(".description")[index]).addClass("future");
       }
       index++;
+      console.log(hour);
+      console.log(index);
     }
-    console.log(hour);
-    console.log(index);
   }
 
-  $(".saveBtn").on("click", function () {
-    // get nearby values
-    var value = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
+  // $(".saveBtn").on("click", function () {
+  //   // get nearby values
+  //   var value = $(this).siblings(".description").val();
+  //   var time = $(this).parent().attr("id");
 
-    // save in localStorage
-    localStorage.setItem(time, value);
-  });
+  //   // save in localStorage
+  //   localStorage.setItem(time, value);
+  // });
   // get current number of hours
-  var currentHour = moment().hours();
+  // var currentHour = moment().hours();
 
-  $("#hour-9 .description").val(localStorage.getItem("hour"));
-  // $("#hour-10 .description").val(localStorage.getItem("hour"));
-  // $("#hour-11 .description").val(localStorage.getItem("hour"));
-  // $("#hour-12 .description").val(localStorage.getItem("hour"));
-  // $("#hour-1 .description").val(localStorage.getItem("hour"));
-  // $("#hour-2 .description").val(localStorage.getItem("hour"));
-  // $("#hour-3 .description").val(localStorage.getItem("hour"));
-  // $("#hour-4 .description").val(localStorage.getItem("hour"));
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
 
   tellTime();
 });
